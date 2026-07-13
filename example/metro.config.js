01f00da6decb6a8007bd668f932cc96cc20abe64
alihamzaazhar <alihamza.azhar124@gmail.com>
2026-07-13 10:37:21 +0500

    Fix example Metro workspace resolution

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,18 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const workspaceRoot = path.resolve(__dirname, '..');
+const config = {
+  watchFolders: [workspaceRoot],
+  resolver: {
+    extraNodeModules: {
+      'react-native-stylus': workspaceRoot,
+    },
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
